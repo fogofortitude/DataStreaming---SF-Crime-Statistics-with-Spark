bc215c72c63885a0fa2492d72fd011f7750fb589
fogofortitude <nick.drewitt.smith@gmail.com>
2020-09-23 09:39:22 +1000

    Completed empty sections of the code
    Provided input values for: 
    Topic Name
    Kafka Bootstrap Server
    Kafka Client Identifier

--- kafka_server.py
+++ kafka_server.py
@@ -1,16 +1,24 @@
 import producer_server
 
+TOPIC_NAME = 'com.sf.police.service.calls'
+SERVER_URL = 'localhost:9092'
+CLIENT_ID = 'com.udacity.dep.police.broker'
 
 def run_kafka_server():
 	# TODO get the json file path
-    input_file = ""
+    input_file = "./police-department-calls-for-service.json"
 
-    # TODO fill in blanks
+    ''' client id: is used as an identifier for the client application. It will be used when logging errors, monitoring aggregates. 
+     	it acts as a logical grouping across all requests from a particular client.
+     	for exmample; one might want to monitor not just the requests per second overall, but the number coming from each client application 
+	(each of which could reside on multiple servers). 
+    '''
+
     producer = producer_server.ProducerServer(
         input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        topic=TOPIC_NAME,
+        bootstrap_servers=SERVER_URL ,
+        client_id=CLIENT_ID  
     )
 
     return producer
