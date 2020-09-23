df92e43c06220188d2955b9f9876815b418735f3
fogofortitude <nick.drewitt.smith@gmail.com>
2020-09-23 10:50:42 +1000

    Updated TODO parts of code and added comments
    updated lines 32
    added lines 17, 20 and 25

--- producer_server.py
+++ producer_server.py
@@ -2,6 +2,8 @@ from kafka import KafkaProducer
 import json
 import time
 
+# Producers produce messages to a topic of their choice. 
+# It is possible to attach a key to each message, in which case the producer guarantees that all messages with the same key will arrive to the same partition.
 
 class ProducerServer(KafkaProducer):
 
@@ -10,16 +12,22 @@ class ProducerServer(KafkaProducer):
         self.input_file = input_file
         self.topic = topic
 
-    #TODO we're generating a dummy data
+    #Generates dummy data from the input file
     def generate_data(self):
-        with open(self.input_file) as f:
-            for line in f:
+        print(self.topic)  # remove after testing
+        with open(self.input_file) as file:
+            # experiment with placement of data = json.load(file) 
+            # data = json.load(file) 
+            
+            for line in file:
                 message = self.dict_to_binary(line)
                 # TODO send the correct data
+                data = json.load(file)  #clarify why we have reassigned to data attribute
+                self.topic, message
                 self.send()
                 time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
-        return 
-        
+        return json.dumps(json_dict).encode('utf-8')
+ 
